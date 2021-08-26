6556d5cc9bf790b6856015a2f75b24dd630efff3
rkrailas <rkrailas@gmail.com>
2021-08-26 10:53:42 +0700

    First Commit

---
+++ resources/views/layouts/partials/aside.blade.php
@@ -0,0 +1,63 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+            </div>
+            <div class="info"></div>
+            <a href="#" class="d-block">Admin</a>
+        </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="{{ route('accstar.customer') }}"  
+                    class="nav-link {{ request()->is('accstar/customer') ? 'active' : '' }}">
+                    <i class="nav-icon far fa-address-book"></i>
+                    <p>ลูกค้า</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('accstar.gljournal') }}" 
+                    class="nav-link {{ request()->is('accstar/gjournal') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-book"></i>
+                    <p>ใบสำคัญ</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('accstar.sodeliverytax') }}" class="nav-link {{ request()->is('accstar/sodeliverytax') ? 'active' : '' }}">
+                    <i class="nav-icon fa fa-shopping-cart"></i>
+                    <p>ส่งสินค้าพร้อมใบกำกับ</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <form method="POST">
+                    @csrf
+                    <a href="{{ route('accstar.sodeliverytax') }}" 
+                        onClick="event.preventDefault(); this.closest('form').submit();" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Logout
+                        </p>
+                    </a>
+                </form>
+
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Test1;
+use App\Http\Livewire\ContactSearchBar;
+use App\Http\Livewire\AccStar\ListGjournal;
+use App\Http\Livewire\AccStar\Customer;
+use App\Http\Livewire\AccStar\CustomerForm;
+use App\Http\Livewire\AccStar\Accounting;
+use App\Http\Livewire\AccStar\Products;
+use App\Http\Livewire\AccStar\SoDeliveryTax;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test1', Test1::class);
+Route::get('contactsearch', ContactSearchBar::class);
+Route::get('accstar/gjournal', ListGjournal::class)->name('accstar.gljournal');
+Route::get('accstar/customer', Customer::class)->name('accstar.customer');
+Route::get('accstar/customer/{customer_id}/edit', CustomerForm::class)->name('accstar.customer.form');
+Route::get('accstar/sodeliverytax', SoDeliveryTax::class)->name('accstar.sodeliverytax');
+Route::get('accstar/accounting', Accounting::class); //Not Used
+Route::get('accstar/products', Products::class); //Not Used
